4fdba4cf6c83677b9a81202951b11409272ab799
Chase Moskal <chasemoskal@gmail.com>
2016-01-31 18:01:02 -0800

    User experience refactor.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //// PasteSafe web app.
 ////
 
+var officialBaseLink = "https://chasemoskal.github.io/PasteSafe/";
+
 // Forcing HTTPS in production.
 var production = /github\.io/i.test(window.location.href) || /pastesafe\.com/i.test(window.location.href);
 if (production && !/https/i.test(window.location.protocol))
@@ -9,13 +11,32 @@ if (production && !/https/i.test(window.location.protocol))
 
 // Obtaining references to PasteSafe DOM elements.
 var root = document.querySelector('[paste-safe]');
-var textInput = root.querySelector('textarea.text-input');
-var textOutput = root.querySelector('textarea.text-output');
-var passwordInput = root.querySelector('input.password');
+var textInput = root.querySelector('.text-input');
+var textOutput = root.querySelector('.text-output');
+var passwordInput = root.querySelector('.password');
+var passwordTooltip = root.querySelector('.passbox .tooltip');
 var encryptButton = root.querySelector('.encrypt');
 var decryptButton = root.querySelector('.decrypt');
 var outputBlocker = root.querySelector('.output-blocker');
 var bottomLink = root.querySelector('.bottom-link');
+
+// Start focused on password box.
+passwordInput.focus();
+
+var setBottomLink = (hex) => {
+    if (hex) {
+        bottomLink.href = "#" + hex;
+        bottomLink.textContent = officialBaseLink + "#" + hex.substring(0, 8) + "...";
+        bottomLink.setAttribute("data-show", "");
+    }
+    else {
+        bottomLink.href = "#";
+        bottomLink.textContent = "";
+        bottomLink.removeAttribute("data-show");
+    }
+};
+setBottomLink();
+
 var getCryptionMode = () => root.querySelector('input[name="cryption_mode"]:checked').value;
 
 // Handling the switch between 'encrypt' and 'decrypt' cryption mode.
@@ -48,6 +69,7 @@ function setCryptionMode(mode) {
 //  - plain: subtle blocker with a simple message.
 //  - error: angry blocker that indicates something went wrong.
 function setOutputBlocker(options) {
+    return false;
     var mode = options.mode || "off";
     var list = options.list || [];
     outputBlocker.setAttribute("data-mode", mode);
@@ -65,20 +87,22 @@ function instantAction() {
     if (instantActionInProgress) return;
     instantActionInProgress = true;
 
+    passwordTooltip.removeAttribute("data-show");
+
     var cryptionMode = getCryptionMode();
 
     if (!!textInput.value && !!passwordInput.value) {
         if (cryptionMode === 'encrypt') {
             aesBuddy.encrypt(passwordInput.value, textInput.value)
                 .then(hex => {
-                    textOutput.value = hex;
-                    bottomLink.textContent = bottomLink.href = "#" + hex;
+                    textOutput.textContent = hex;
+                    setBottomLink(hex);
                     return hex;
                 })
                 .then(hex => { setOutputBlocker({mode: "off", list: []}); })
                 .catch(error => {
-                    textOutput.value = "";
-                    bottomLink.textContent = bottomLink.href = "#";
+                    textOutput.textContent = "";
+                    setBottomLink();
                     setOutputBlocker({mode: "error", list: ["encryption error"]});
                 })
                 .then(()=>{ instantActionInProgress = false; });
@@ -86,22 +110,22 @@ function instantAction() {
         else {
             aesBuddy.decrypt(passwordInput.value, textInput.value)
                 .then(text => {
-                    textOutput.value = text;
-                    bottomLink.textContent = bottomLink.href = "#";
+                    textOutput.textContent = text;
+                    setBottomLink();
                     return text;
                 })
                 .then(text => { setOutputBlocker({mode: "off", list: []}) })
                 .catch(error => {
-                    textOutput.value = "";
-                    bottomLink.textContent = bottomLink.href = "#";
+                    textOutput.textContent = "";
+                    setBottomLink();
                     setOutputBlocker({mode: "error", list: ["invalid"]});
                 })
                 .then(()=>{ instantActionInProgress = false; });
         }
     }
     else {
-        textOutput.value = "";
-        bottomLink.textContent = bottomLink.href = "#";
+        textOutput.textContent = "";
+        setBottomLink();
         setOutputBlocker({mode: "plain", list: ["enter some input text and a password above"]});
         instantActionInProgress = false;
     }
@@ -115,16 +139,16 @@ decryptButton.addEventListener('change', instantAction);
 
 // Flyout toggle.
 var plate = document.querySelector('.plate');
-plate.setAttribute("data-flyout", window.localStorage.getItem("flyout") || "active");
+plate.setAttribute("data-flyout-state", window.localStorage.getItem("flyout") || "active");
 function toggleHandler(event) {
-    var currentStatus = plate.getAttribute("data-flyout");
-    var newStatus = currentStatus==="active" ?"hidden" :"active";
-    plate.setAttribute("data-flyout", newStatus);
-    window.localStorage.setItem("flyout", newStatus);
+    var currentState = plate.getAttribute("data-flyout-state");
+    var newState = currentState==="active" ?"hidden" :"active";
+    plate.setAttribute("data-flyout-state", newState);
+    window.localStorage.setItem("flyout", newState);
     if (event) event.preventDefault();
     return false;
 }
-var toggleButtons = document.querySelectorAll('.flyout-toggle');
+var toggleButtons = document.querySelectorAll('.flyout-toggle-button');
 for (var i=0; i<toggleButtons.length; i++)
     toggleButtons[i].onclick = toggleHandler;
 
@@ -137,6 +161,7 @@ function interpretHash() {
         setCryptionMode('decrypt');
         passwordInput.focus();
         instantAction();
+        passwordTooltip.setAttribute("data-show", "");
     }
     window.location.hash = ''; // Removing hash afterwards.
 }
