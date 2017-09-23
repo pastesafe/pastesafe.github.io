d91c883e610361180437f0d56fec76aa93617fc4
Chase Moskal <lordbrimshaw@gmail.com>
2017-09-23 15:54:01 -0700

    simplify pastesafe component

--- source/components/pastesafe/pastesafe.tsx
+++ source/components/pastesafe/pastesafe.tsx
@@ -7,32 +7,13 @@ import PasteSafeStore from "./pastesafe-store"
 export default observer(({store}: {store: PasteSafeStore}) =>
 	<div className="pastesafe">
 
-		<div className="controls">
-			<label className="cell mode-button" htmlFor="encrypt" data-checked>
-				<input id="encrypt" className="encrypt" type="radio" name="cryption_mode" value="encrypt" checked/>
-				Encrypt
-			</label>
-			<label className="cell mode-button" htmlFor="decrypt">
-				<input id="decrypt" className="decrypt" type="radio" name="cryption_mode" value="decrypt"/>
-				Decrypt
-			</label>
-			<div className="cell">
-				<div className="passbox">
-					{/*avoid the word "password" on the input, to hide from pesky password managers.*/}
-					<input className="key" type="text" placeholder="Key" autoComplete="off" maxLength={32}/>
-					<div className="tooltip">
-						<p>Enter the password here to decrypt the message!</p>
-					</div>
-				</div>
-			</div>
-		</div>
+		{/* avoid the word "password" on the input, to hide from pesky password managers */}
+		<input className="key" type="text" placeholder="Key" autoComplete="off" maxLength={32}/>
 
 		<textarea className="text-input" placeholder="Secret message"></textarea>
 
 		<textarea className="text-output" readOnly></textarea>
 
-		<div className="linkbox">
-			<a className="bottom-link" title="Shareable link" href="#"></a>
-		</div>
+		<a className="share-link" title="Shareable link" href="#"></a>
 	</div>
 )
