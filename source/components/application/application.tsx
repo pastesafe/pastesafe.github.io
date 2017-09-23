c5887a28ac33a106c0906826305df11493cc91dc
Chase Moskal <lordbrimshaw@gmail.com>
2017-09-23 15:03:44 -0700

    install react and mobx

---
+++ source/components/application/application.tsx
@@ -0,0 +1,9 @@
+
+import * as React from "react"
+import {observer} from "mobx-react"
+
+export default observer(() =>
+	<div className="application">
+		<h1>pastesafe — v0.12.0-dev</h1>
+	</div>
+)
---
+++ source/script.tsx
@@ -0,0 +1,7 @@
+
+import * as React from "react"
+import * as ReactDom from "react-dom"
+
+import Application from "./components/application/application"
+
+ReactDom.render(<Application/>, document.querySelector("body > .container"))
